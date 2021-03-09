5a15b4a013e15c39b3857c85e08da974b478b42a
benoitba <benoit.baillif@gmail.com>
2021-03-09 15:18:29 +0000

    change in script to save hyperparams in checkpoint, added generation notebook on trained models

--- LitCoordAE.py
+++ LitCoordAE.py
@@ -7,6 +7,7 @@ from pytorch_lightning.core.lightning import LightningModule
 from torch.optim import Adam
 from MSDScorer import MSDScorer
 from KLDLoss import KLDLoss
+from argparse import ArgumentParser
 
 from rdkit import Chem
 from rdkit.Chem import AllChem
@@ -16,61 +17,44 @@ from rdkit.Chem import AllChem
 # 3D Coordinates autoencoder model
 class LitCoordAE(LightningModule):
 
-    def __init__(self, n_max, dim_node, dim_edge, dim_h, dim_f, \
-                batch_size, mpnn_steps=5, alignment_type='kabsch2', \
-                use_X=True, use_R=True, useFF=False, w_reg = 1e-5, \
-                refine_steps=0, refine_mom=0.99, val_num_samples=10):
+    def __init__(self, hparams, *args, **kwargs):
         
         super(LitCoordAE, self).__init__()
         
-        self.mpnn_steps = mpnn_steps
-        self.n_max = n_max
-        self.dim_node = dim_node
-        self.dim_edge = dim_edge
-        self.dim_h = dim_h
-        self.dim_f = dim_f
-        self.batch_size = batch_size
-        self.val_num_samples = val_num_samples
-        self.refine_steps = refine_steps
-        self.refine_mom = refine_mom
-        self.use_X = use_X # Include position in the node embedding
-        self.use_R = use_R # Include distance in the edge embedding
-        self.kldloss = KLDLoss()
-        self.msd_scorer = MSDScorer(alignment_type=alignment_type)
-        self.useFF = useFF
-        self.w_reg = w_reg
-            
-        #self.G not included
-        #placeholders not included
-        #prior_T not included
+        self.hparams = hparams # store all the hyperparameters of the model. 
+        # See argument parser (model and in training script to find the parameters and a short description)
         
-        # ADD SELF IN ARGS FOR FOLLOWING LINES
-        self.embed_nodes = EmbedNode(batch_size, n_max, dim_node, dim_h)
+        self.kldloss = KLDLoss()
+        self.msd_scorer = MSDScorer(alignment_type=hparams.alignment_type)
+        
+        # Difference from original model : self.G not included ; prior_T not included
+        
+        self.embed_nodes = EmbedNode(hparams.batch_size, hparams.n_max, hparams.dim_node, hparams.dim_h)
         
         # Prior Z
-        self.edge_nn_prior_z = EdgeNN(batch_size, n_max, dim_edge + 1, dim_h)
-        self.mpnn_prior_z = MPNN(batch_size, n_max, dim_h, mpnn_steps)
-        self.latent_nn_prior_z = LatentNN(batch_size, n_max, dim_h, dim_f, 2*dim_h)
+        self.edge_nn_prior_z = EmbedEdge(hparams.batch_size, hparams.n_max, hparams.dim_edge + 1, hparams.dim_h)
+        self.mpnn_prior_z = MPNN(hparams.batch_size, hparams.n_max, hparams.dim_h, hparams.mpnn_steps)
+        self.latent_nn_prior_z = LatentNN(hparams.batch_size, hparams.n_max, hparams.dim_h, hparams.dim_f, 2*hparams.dim_h)
         
         # Post Z
-        if self.use_R :
-            self.edge_nn_post_z = EdgeNN(batch_size, n_max, dim_edge + 2, dim_h)
+        if self.hparams.use_R :
+            self.edge_nn_post_z = EmbedEdge(hparams.batch_size, hparams.n_max, hparams.dim_edge + 2, hparams.dim_h)
         else :
-            self.edge_nn_post_z = EdgeNN(batch_size, n_max, dim_edge + 1, dim_h)
+            self.edge_nn_post_z = EmbedEdge(hparams.batch_size, hparams.n_max, hparams.dim_edge + 1, hparams.dim_h)
             
-        if use_X:
-            self.embed_nodes_pos = EmbedNode(batch_size, n_max, dim_node + 3, dim_h)
+        if self.hparams.use_X:
+            self.embed_nodes_pos = EmbedNode(hparams.batch_size, hparams.n_max, hparams.dim_node + 3, hparams.dim_h)
             
-        self.mpnn_post_z = MPNN(batch_size, n_max, dim_h, mpnn_steps)
-        self.latent_nn_post_z = LatentNN(batch_size, n_max, dim_h, dim_f, 2*dim_h)
+        self.mpnn_post_z = MPNN(hparams.batch_size, hparams.n_max, hparams.dim_h, hparams.mpnn_steps)
+        self.latent_nn_post_z = LatentNN(hparams.batch_size, hparams.n_max, hparams.dim_h, hparams.dim_f, 2*hparams.dim_h)
         
         # Post X
-        self.edge_nn_post_x = EdgeNN(batch_size, n_max, dim_edge + 1, dim_h)
-        self.mpnn_post_x = MPNN(batch_size, n_max, dim_h, mpnn_steps)
-        self.latent_nn_post_x = LatentNN(batch_size, n_max, dim_h, dim_f, 3)
+        self.edge_nn_post_x = EmbedEdge(hparams.batch_size, hparams.n_max, hparams.dim_edge + 1, hparams.dim_h)
+        self.mpnn_post_x = MPNN(hparams.batch_size, hparams.n_max, hparams.dim_h, hparams.mpnn_steps)
+        self.latent_nn_post_x = LatentNN(hparams.batch_size, hparams.n_max, hparams.dim_h, hparams.dim_f, 3)
         
 #         # Pred X
-#         self.edge_nn_pred_x = EdgeNN(batch_size, n_max, dim_edge + 1, dim_h)
+#         self.edge_nn_pred_x = EmbedEdge(batch_size, n_max, dim_edge + 1, dim_h)
 #         self.mpnn_pred_x = MPNN(batch_size, n_max, dim_h, mpnn_steps)
 #         self.latent_nn_pred_x = LatentNN(batch_size, n_max, dim_h, dim_f, 3)
         
@@ -90,7 +74,7 @@ class LitCoordAE(LightningModule):
 
         # Isn't there a better way to add n_atom in edge features ?
         n_atom = masks.permute(0, 2, 1).sum(2) # (batch_size, 1)
-        tiled_n_atom = n_atom.view(-1, 1, 1, 1).repeat(1, self.n_max, self.n_max, 1) # (batch_size, n_max, n_max, 1)
+        tiled_n_atom = n_atom.view(-1, 1, 1, 1).repeat(1, self.hparams.n_max, self.hparams.n_max, 1) # (batch_size, n_max, n_max, 1)
         edge_2 = torch.cat([edges, tiled_n_atom], 3) # (batch_size, n_max, nmax, dim_edge + 1)
 
 
@@ -100,20 +84,20 @@ class LitCoordAE(LightningModule):
         priorZ_hidden = self.mpnn_prior_z(priorZ_edge_wgt, nodes_embed, masks) # (batch_size, n_max, dim_h), nodes_embed like
         priorZ_out = self.latent_nn_prior_z(priorZ_hidden, nodes_embed, masks) # (batch_size, n_max, 2*dim_h)
 
-        priorZ_mu, priorZ_lsgms = priorZ_out.split([self.dim_h, self.dim_h], 2)
+        priorZ_mu, priorZ_lsgms = priorZ_out.split([self.hparams.dim_h, self.hparams.dim_h], 2)
         priorZ_sample = self._draw_sample(priorZ_mu, priorZ_lsgms, masks) # (batch_size, n_max, dim_h)
 
 
         # q(Z|R(X),G) -- posterior of Z
 
-        if self.use_R:
-            proximity_view = proximity.view(-1, self.n_max, self.n_max, 1)
+        if self.hparams.use_R:
+            proximity_view = proximity.view(-1, self.hparams.n_max, self.hparams.n_max, 1)
             edge_cat = torch.cat([edge_2, proximity_view], 3) #[batch_size, n_max, n_max, dim_edge + 2]
             postZ_edge_wgt = self.edge_nn_post_z(edge_cat) #[batch_size, n_max, n_max, dim_h, dim_h]
         else:
             postZ_edge_wgt = self.edge_nn_post_z(edge_2) 
 
-        if self.use_X:
+        if self.hparams.use_X:
             nodes_pos = torch.cat([nodes, pos], 2) # (batch_size, n_max, dim_node + 3)
             nodes_pos_embed = self.embed_nodes_pos(nodes_pos, masks)
             postZ_hidden = self.mpnn_post_z(postZ_edge_wgt, nodes_pos_embed, masks)
@@ -122,7 +106,7 @@ class LitCoordAE(LightningModule):
 
         postZ_out = self.latent_nn_prior_z(postZ_hidden, nodes_embed, masks)
 
-        postZ_mu, postZ_lsgms = postZ_out.split([self.dim_h, self.dim_h], 2)
+        postZ_mu, postZ_lsgms = postZ_out.split([self.hparams.dim_h, self.hparams.dim_h], 2)
         postZ_sample = self._draw_sample(postZ_mu, postZ_lsgms, masks)
 
 
@@ -143,10 +127,6 @@ class LitCoordAE(LightningModule):
 
         # Prediction of X with p(Z|G) in the test phase
 
-#         PX_edge_wgt = self.edge_nn_pred_x(edge_2) #[batch_size, n_max, n_max, dim_h, dim_h]
-#         PX_hidden = self.mpnn_pred_x(PX_edge_wgt, priorZ_sample + nodes_embed, masks)
-#         PX_pred = self.latent_nn_pred_x(PX_hidden, nodes_embed, masks)
-
         PX_edge_wgt = self.edge_nn_post_x(edge_2) #[batch_size, n_max, n_max, dim_h, dim_h]
         PX_hidden = self.mpnn_post_x(PX_edge_wgt, priorZ_sample + nodes_embed, masks)
         PX_pred = self.latent_nn_post_x(PX_hidden, nodes_embed, masks)
@@ -165,7 +145,7 @@ class LitCoordAE(LightningModule):
         cost_KLDZ = torch.mean(torch.sum(self.kldloss.loss(masks, postZ_mu, postZ_lsgms,  priorZ_mu, priorZ_lsgms), (1, 2))) # posterior | prior
         cost_KLD0 = torch.mean(torch.sum(self.kldloss.loss(masks, priorZ_mu, priorZ_lsgms), (1, 2))) # prior | N(0,1)
         cost_X = torch.mean(self.msd_scorer.score(X_pred, pos, masks))
-        cost_op = cost_X + cost_KLDZ + self.w_reg * cost_KLD0
+        cost_op = cost_X + cost_KLDZ + self.hparams.w_reg * cost_KLD0
 
         self.log("train/cost_op", cost_op)
         self.log("train/cost_X", cost_X)
@@ -181,25 +161,25 @@ class LitCoordAE(LightningModule):
         #masks = masks.unsqueeze(-1)
 
         # repeat because we want val_batch_size (molecule) * val_num_samples (conformer per molecule)
-        nodes = torch.repeat_interleave(nodes, self.val_num_samples, dim=0)
-        masks = torch.repeat_interleave(masks, self.val_num_samples, dim=0)
-        edges = torch.repeat_interleave(edges, self.val_num_samples, dim=0)
-        proximity = torch.repeat_interleave(proximity, self.val_num_samples, dim=0)
+        nodes = torch.repeat_interleave(nodes, self.hparams.val_num_samples, dim=0)
+        masks = torch.repeat_interleave(masks, self.hparams.val_num_samples, dim=0)
+        edges = torch.repeat_interleave(edges, self.hparams.val_num_samples, dim=0)
+        proximity = torch.repeat_interleave(proximity, self.hparams.val_num_samples, dim=0)
 
         _, _, _, _, _, PX_pred = self(nodes, masks, edges, proximity, pos)
 
         X_pred = PX_pred
-        for r in range(self.refine_steps):
+        for r in range(self.hparams.refine_steps):
             if use_X:
                 pos = X_pred
             if use_R:
                 proximity = pos_to_proximity(X_pred, masks)
             _, _, _, _, last_X_pred, _ = self(nodes, masks, edges, proximity, pos)
-            X_pred = self.refine_mom * X_pred + (1-self.refine_mom) * last_X_pred
+            X_pred = self.hparams.refine_mom * X_pred + (1-self.hparams.refine_mom) * last_X_pred
 
         rmsds=[]
         for j in range(X_pred.shape[0]):
-            ms_v_index = int(j / self.val_num_samples)
+            ms_v_index = int(j / self.hparams.val_num_samples)
             rmsds.append(self.getRMSD(mols[ms_v_index], X_pred[j]))
 
         rmsds = np.array(rmsds)
@@ -265,7 +245,7 @@ class LitCoordAE(LightningModule):
         test_mol.RemoveConformer(0)
         test_mol.AddConformer(test_cf)
 
-        if self.useFF:
+        if self.hparams.useFF:
             try:
                 rmsd = AllChem.AlignMol(reference_mol, optimizeWithFF(test_mol))
             except:
@@ -285,6 +265,24 @@ class LitCoordAE(LightningModule):
         sample = torch.mul(sample, masks)
 
         return sample
+    
+    
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument('--alignment_type', type=str, default='kabsch', choices=['default', 'linear', 'kabsch'])
+        parser.add_argument('--seed', type=int, default=1334, help='random seed for experiments')
+        parser.add_argument('--use_X', action='store_true', help='use X as input for posterior of Z')
+        parser.add_argument('--use_R', action='store_true', help='use R(X) as input for posterior of Z')
+        parser.add_argument('--w_reg', type=float, default=1e-5, help='weight for conditional prior regularization')
+        parser.add_argument('--refine_mom', type=float, default=0.99, help='momentum used for refinement')
+        parser.add_argument('--refine_steps', type=int, default=0, help='number of refinement steps if requested')
+        parser.add_argument('--useFF', action='store_true', help='use force field minimisation if testing')
+        parser.add_argument('--dim_h', type=int, default=50, help='dimension of the hidden layer')
+        parser.add_argument('--dim_f', type=int, default=100, 
+                            help='dimension of the last hidden layer before embedding (mol or latent space)')
+        parser.add_argument('--mpnn_steps', type=int, default=5, help='number of mpnn steps')
+        return parser
     
 class EmbedNode(nn.Module):
     
@@ -320,11 +318,11 @@ class EmbedNode(nn.Module):
 
         return nodes_embed
     
-class EdgeNN(nn.Module):
+class EmbedEdge(nn.Module):
     
     def __init__(self, batch_size, n_max, edge_dim, hidden_dim):
         
-        super(EdgeNN, self).__init__()
+        super(EmbedEdge, self).__init__()
         
         self.batch_size = batch_size
         self.n_max = n_max
--- train.py
+++ train.py
@@ -15,76 +15,27 @@ from data_utils import CODDataModule
 def main(args):
     
     data_dir = '/home/bb596/rds/hpc-work/dl4chem/'
-    dim_edge = 10
+    args.dim_edge = 10
     
-    seed = args.seed
-    seed_everything(seed)
+    seed_everything(args.seed)
 
-    dim_h = args.dim_h
-    dim_f = args.dim_f
-    batch_size = args.batch_size
-    val_num_samples = args.val_num_samples
-    alignment_type = args.alignment_type
-    use_X = args.use_X 
-    use_R = args.use_R 
-    refine_steps = args.refine_steps
-    refine_mom = args.refine_mom
-    useFF = args.useFF
-    w_reg = args.w_reg
-    dataset = args.dataset
-    
+    if args.dataset == 'QM9' :
+        args.mpnn_steps = 3
+        args.n_max = 9
+        args.dim_node = 22
 
-    if dataset == 'QM9' :
-        mpnn_steps = 3
-        n_max = 9
-        dim_node = 22
-    else :
-        mpnn_steps = args.mpnn_steps
-        n_max = 50
-        dim_node = 35
-
-    data_module = CODDataModule(dataset=dataset, 
+    data_module = CODDataModule(dataset=args.dataset, 
                                 data_dir=data_dir, 
-                                batch_size=batch_size, 
-                                val_num_sample=val_num_samples)
+                                batch_size=args.batch_size, 
+                                val_num_sample=args.val_num_samples)
 
     if args.checkpoint_start is None :
-        model = LitCoordAE(n_max=n_max,
-                           dim_node=dim_node,
-                           dim_edge=dim_edge, 
-                           dim_h=dim_h, 
-                           dim_f=dim_f, 
-                           batch_size=batch_size, 
-                           mpnn_steps=mpnn_steps, 
-                           alignment_type=alignment_type, 
-                           use_X=use_X, 
-                           use_R=use_R,
-                           useFF=useFF,
-                           refine_steps=refine_steps, 
-                           refine_mom=refine_mom, 
-                           w_reg=w_reg,
-                           val_num_samples=val_num_samples)
-
+        model = LitCoordAE(args)
     else :
-        model = LitCoordAE.load_from_checkpoint(args.checkpoint_start, strict=False,
-                                                n_max=n_max,
-                                                dim_node=dim_node, 
-                                                dim_edge=dim_edge,
-                                                dim_h=dim_h,
-                                                dim_f=dim_f,
-                                                batch_size=batch_size, 
-                                                mpnn_steps=mpnn_steps, 
-                                                alignment_type=alignment_type, 
-                                                use_X=use_X, 
-                                                use_R=use_R,
-                                                useFF=useFF,
-                                                refine_steps=refine_steps, 
-                                                refine_mom=refine_mom, 
-                                                w_reg=w_reg,
-                                                val_num_samples=val_num_samples)
+        model = LitCoordAE.load_from_checkpoint(args.checkpoint_start, strict=False)
 
     tb_logger = pl_loggers.TensorBoardLogger('logs/')
-    trainer = Trainer(logger=tb_logger, gpus=-1, num_nodes=2, accelerator='ddp')
+    trainer = Trainer.from_argparse_args(args, logger=tb_logger)
     trainer.fit(model, data_module)
 
 
@@ -93,21 +44,13 @@ if __name__ == '__main__':
     parser = ArgumentParser(description='Train network')
     
     parser.add_argument('--dataset', type=str, default='QM9', choices=['COD', 'QM9', 'CSD'])
-    parser.add_argument('--alignment_type', type=str, default='kabsch', choices=['default', 'linear', 'kabsch'])
-    parser.add_argument('--seed', type=int, default=1334, help='random seed for experiments')
     parser.add_argument('--batch_size', type=int, default=20, help='batch size')
     parser.add_argument('--val_num_samples', type=int, default=10,
                         help='number of samples from prior used for validation')
-    parser.add_argument('--use_X', action='store_true', help='use X as input for posterior of Z')
-    parser.add_argument('--use_R', action='store_true', help='use R(X) as input for posterior of Z')
-    parser.add_argument('--w_reg', type=float, default=1e-5, help='weight for conditional prior regularization')
-    parser.add_argument('--refine_mom', type=float, default=0.99, help='momentum used for refinement')
-    parser.add_argument('--refine_steps', type=int, default=0, help='number of refinement steps if requested')
-    parser.add_argument('--useFF', action='store_true', help='use force field minimisation if testing')
-    parser.add_argument('--dim_h', type=int, default=50, help='dimension of the hidden')
-    parser.add_argument('--dim_f', type=int, default=100, help='dimension of the hidden')
-    parser.add_argument('--mpnn_steps', type=int, default=5, help='number of mpnn steps')
-    parser.add_argument('--checkpoint_start', type=str, default=None, help='checkpoint to the model on')
+    parser.add_argument('--checkpoint_start', type=str, default=None, help='checkpoint to start the model on')
+    
+    parser = LitCoordAE.add_model_specific_args(parser)
+    parser = Trainer.add_argparse_args(parser) # add the gpus and num_nodes arguments for multi-nodes training on cluster
 
     args = parser.parse_args()
 
